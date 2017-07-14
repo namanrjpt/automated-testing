3324a41068ebde5a46681e9401de565d0a60f2b2
Naman Rajput <mail.namanrajput@gmail.com>
2017-07-14 11:35:07 +0530

    Added get Help Now Tests

---
+++ PCSAWebTests/PCSAWebTests/src/GetHelpNowTests/officeOfVictimAdvocacy.java
@@ -0,0 +1,72 @@
+package GetHelpNowTests;
+
+import LoginAndRegistration.existingUserLogin;
+import Logs.log4j;
+import PageObjectModel.getHelpNowPageElements;
+import PageObjectModel.sideInMenuBarElements;
+import WebDriver.driver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by naman on 11/7/17.
+ */
+public class officeOfVictimAdvocacy {
+
+    @BeforeClass
+    public static void startDriver()
+    {
+        try
+        {
+            driver.initalizeDriver();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        existingUserLogin.login();
+    }
+
+    @Test(priority = 0)
+    public void canGoToOfficeOfVictimAdvocacy()
+    {
+        sideInMenuBarElements.getHelpNowTab().click();
+        log4j.Log.info("canGoToGetHelpNow starting");
+        try{
+            driver.waitDriverForElement(getHelpNowPageElements.gethelpPageTitle());
+        }
+        catch(IOException e){
+            log4j.Log.info(e);
+        }
+
+        getHelpNowPageElements.officeOfVictimAdvocacy().click();
+
+        try{
+            driver.waitDriverForElement(getHelpNowPageElements.voiceCallButton());
+        }
+        catch(IOException e){
+            log4j.Log.info(e);
+        }
+
+        Assert.assertEquals(getHelpNowPageElements.voiceCallButton().isDisplayed() && getHelpNowPageElements.sendMessageButton().isDisplayed(), true, "Contact PCMO page is not loading!");
+
+    }
+
+    @AfterClass
+    public static void tearDown()
+    {
+        try
+        {
+            driver.closeDriver();
+        }
+        catch( Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
